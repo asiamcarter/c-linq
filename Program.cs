e034d59cb535363833ace16a0392e973f76188a3
Asia Carter <asiamcarter@gmail.com>
2019-03-11 15:46:03 -0500

    working through assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace c_linq
 {
@@ -6,7 +8,69 @@ namespace c_linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+            IEnumerable<string> LFruits = from fruit in fruits
+                where fruit[0] == 'L'
+                select fruit;
+                // Console.WriteLine("Fruits That Begin With 'L':");
+                // Console.WriteLine("----------");
+                // foreach(string f in LFruits) {
+                // Console.WriteLine(f);
+                // }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(multiple=> multiple % 4 == 0 | multiple % 6 == 0 );
+            // foreach(int multiple in fourSixMultiples) {
+            //     Console.WriteLine(multiple);
+            // }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            List<string> descend = names.OrderByDescending(name=>name).ToList();
+            //      foreach(string name in descend) {
+            //     Console.WriteLine(name);
+            // }
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers2 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+            List<int> ascend = numbers2.OrderBy(num1=>num1).ToList();
+            //       foreach(int num in ascend) {
+            //     Console.WriteLine (num);
+            // }
+            // Output how many numbers are in this list
+
+            List<int> numbers3 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
         }
     }
+
 }
